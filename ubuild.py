b2588b420565b0201ba53ce7d8ce4f911d429bb1
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2018-03-13 07:38:26 -0700

    Adding build infrastructure
    * adding uranium scaffolding
    * adding travis ci integration

--- ubuild.py
+++ ubuild.py
@@ -1,61 +1,51 @@
 import os
-from setuptools import setup, find_packages
+import subprocess
+from uranium import current_build
 
-base = os.path.dirname(os.path.abspath(__file__))
+current_build.config.set_defaults({
+    "module": "orbital_core"
+})
 
 
-def read(fname):
-    '''Utility function to read the README file.'''
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+MODULE_NAME = "orbital_core"
 
-install_requires = [
-    'arrow',
-    'aiohttp-transmute',
-    'aiohttp_cors',
-    'docopt',
-    'jinja2',
-]
+def main(build):
+    build.packages.install(".", develop=True)
 
-data_files = []
 
-target = os.path.join(base, "zillow_aiohttp", "templates")
-for root, dirs, files in os.walk(target):
-    relative_root = root[len(target):]
-    relative_root = relative_root.lstrip("/")
-    for d in dirs:
-        full_path = os.path.join(relative_root, "*")
-        data_files.append(full_path)
+def test(build):
+    main(build)
+    build.packages.install("jedi")
+    build.packages.install("pytest")
+    build.packages.install("pytest-cov")
+    build.packages.install("pytest-aiohttp")
+    pytest = os.path.join(build.root, "bin", "py.test")
+    subprocess.call([
+        pytest, "--cov", build.config["module"],
+        f"{build.config['module']}/tests",
+        "--cov-report", "term-missing"
+    ] + build.options.args)
 
-route_templates = os.path.join(base, "zillow_aiohttp", "routes", "templates")
-for root, dirs, files in os.walk(route_templates):
-    relative_root = root[len(route_templates):]
-    relative_root = relative_root.lstrip("/")
-    for d in dirs:
-        full_path = os.path.join(relative_root, "*")
-        data_files.append(full_path)
 
-setup(
-    name="zillow-aiohttp",
-    setup_requires=["vcver", "setuptools-parcels"],
-    vcver={"path": base},
-    author="mondev",
-    author_email="mondev@zillowgroup.com",
-    description="common code for orbital services",
-    keywords="zillow",
-    url="https://stash.atl.zillow.net/projects/LIBS/repos/egg.zillow-aiohttp/browse",
-    packages=find_packages(),
-    long_description=read('README.md'),
-    classifiers=[
-        "Topic :: Utilities",
-    ],
-    install_requires=install_requires,
-    tests_require=[] + install_requires,
-    entry_points={
-        'console_scripts': [
-            'install_zillow_aiohttp_templates=zillow_aiohttp.scripts.install_templates:main',
-            'generate_swagger_json=zillow_aiohttp.scripts.generate_swagger_json:main',
-        ]
-    },
-    package_data={"zillow_aiohttp": data_files},
-    include_package_data=True,
-)
+def publish(build):
+    """ distribute the uranium package """
+    build.packages.install("wheel")
+    build.executables.run([
+        "python", "setup.py",
+        "sdist", "bdist_wheel", "--universal", "upload", "--release"
+    ])
+
+
+def stamp(build):
+    """ after a distribution, stamp the current build. """
+    build.packages.install("gitchangelog")
+    changelog_text = subprocess.check_output(["gitchangelog"])
+    with open(os.path.join(build.root, "CHANGELOG"), "wb+") as fh:
+        fh.write(changelog_text)
+
+
+def build_docs(build):
+    build.packages.install("sphinx")
+    return subprocess.call(
+        ["make", "html"], cwd=os.path.join(build.root, "docs")
+    )
