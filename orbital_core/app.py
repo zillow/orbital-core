e44b6bc5acc86c530046c84b3708439b3f25f493
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2018-03-13 21:01:51 -0700

    have unit tests working.

--- orbital_core/app.py
+++ orbital_core/app.py
@@ -1,3 +1,4 @@
+from .routes import add_routes
 
 def bootstrap_app(app, root_dir=None,
                   service_name="my service",
@@ -8,17 +9,11 @@ def bootstrap_app(app, root_dir=None,
         configures the percentage that is required to capture that stack
         for flamegraph analysis.
     """
-    app[SHUTDOWN_KEY] = False
     app["service_name"] = service_name
     app["service_description"] = service_description
-    # app["cpu_capture"] = CpuStackCollector(min_cpu_percent_for_capture)
     app.on_startup.append(on_startup)
     add_routes(app, root_dir)
-    _add_signal_handler(app)
-    if app.get('config', {}).get('zdc'):
-        _init_zdc_metrics(app)
 
 
 async def on_startup(app):
     pass
-    #app["cpu_capture"].start_polling()
--- orbital_core/tests/conftest.py
+++ orbital_core/tests/conftest.py
@@ -1,1 +1,1 @@
-pytest_plugins = 'aiohttp.pytest_plugin'
+        
