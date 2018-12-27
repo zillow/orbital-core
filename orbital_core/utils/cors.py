af18ac720c3d6b66a681fc8911fd298e5fe1e0a7
Yusuke Tsutsumi <yusuket@zillowgroup.com>
2018-12-27 13:03:30 -0800

    Adding simple enabler of cors.

---
+++ orbital_core/utils/cors.py
@@ -0,0 +1,26 @@
+def enable_cors(app):
+    """
+    Globally enable CORS.
+
+    This is an unsafe operation for sensitive services,
+    as it allows any website to perform actions on this service
+    with the user's credentials.
+
+    This is a quick workaround that might be more acceptible for an
+    internal tool.
+
+    This function should be called after all other routes are added.
+    """
+    # Configure default CORS settings.
+    cors = aiohttp_cors.setup(app, defaults={
+        "*": aiohttp_cors.ResourceOptions(
+                allow_credentials=True,
+                expose_headers="*",
+                allow_headers="*",
+            )
+    })
+
+    # Configure CORS on all routes.
+    for route in list(app.router.routes()):
+        if not isinstance(route.resource, web.StaticResource):
+            cors.add(route)
