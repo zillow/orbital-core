a440564246cc49df8c9476b3afed4de9ebc90690
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2020-01-17 15:07:32 -0800

    build publish: using build.root in publish

--- orbital_core/build/tasks.py
+++ orbital_core/build/tasks.py
@@ -2,10 +2,12 @@ import os
 import multiprocessing
 import shutil
 
+
 def main(build):
     build.packages.install(".", develop=True)
     build.run_task("build_docs")
     build.run_task("copy_docs")
+
 
 def test(build):
     build.packages.install("jedi")
@@ -14,24 +16,36 @@ def test(build):
     build.packages.install("pytest-xdist")
     build.packages.install("pytest-aiohttp")
     build.packages.install("pytest-runfailed")
-    build.executables.run([
-        "py.test", "--cov", build.config["module"],
-        os.path.join(build.root, build.config["module"], "tests"),
-        "--cov-report", "term-missing",
-        "-n", "{0}".format(multiprocessing.cpu_count())
-    ] + build.options.args)
+    build.executables.run(
+        [
+            "py.test",
+            "--cov",
+            build.config["module"],
+            os.path.join(build.root, build.config["module"], "tests"),
+            "--cov-report",
+            "term-missing",
+            "-n",
+            "{0}".format(multiprocessing.cpu_count()),
+        ]
+        + build.options.args
+    )
+
 
 def publish(build):
     """ publish the package itself """
     build.packages.install("wheel")
     build.packages.install("twine")
-    build.executables.run([
-        "python", "setup.py",
-        "sdist", "bdist_wheel", "--universal", "--release"
-    ])
-    build.executables.run([
-        "twine", "upload", "dist/*"
-    ])
+    build.executables.run(
+        [
+            "python",
+            os.path.join(build.root, "setup.py"),
+            "sdist",
+            "bdist_wheel",
+            "--universal",
+            "--release",
+        ]
+    )
+    build.executables.run(["twine", "upload", "dist/*"])
 
 
 def stamp(build):
@@ -41,14 +55,18 @@ def stamp(build):
     with open(os.path.join(build.root, "CHANGELOG"), "wb+") as fh:
         fh.write(changelog_text)
 
+
 def build_docs(build):
     build.packages.install("sphinx")
     build.packages.install("sphinx_rtd_theme")
-    build.executables.run([
-        "sphinx-build",
-        os.path.join(build.root, "docs"),
-        os.path.join(build.root, "target", "docs")
-    ])
+    build.executables.run(
+        [
+            "sphinx-build",
+            os.path.join(build.root, "docs"),
+            os.path.join(build.root, "target", "docs"),
+        ]
+    )
+
 
 def copy_docs(build):
     """ copy documentation into the application directory. This allows
@@ -59,5 +77,5 @@ def copy_docs(build):
         shutil.rmtree(doc_dir)
     shutil.copytree(
         os.path.join(build.root, "target", "docs"),
-        os.path.join(build.root, build.config["module"], "docs")
+        os.path.join(build.root, build.config["module"], "docs"),
     )
